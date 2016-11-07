3ae2b00da9a15845b539a60b9f2fb546e7805715
Michael Mangrobang <micky.jm4@gmail.com>
2016-11-07 10:35:19 -1000

    copied mockup files over

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,9 +2,37 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('Landing', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/home', {
   name: 'Home_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Home_Page' });
+  },
+});
+
+FlowRouter.route('/login', {
+  name: 'Log_In_Page',
+  action() {
+    BlazeLayout.render('Landing', { main: 'Log_In_Page' });
+  },
+});
+
+FlowRouter.route('/user', {
+  name: 'User_Profile_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'User_Profile_Page' });
+  },
+});
+
+FlowRouter.route('/user/mjm4', {
+  name: 'User_Profile_Page_Example',
+  action() {
+    BlazeLayout.render('AppExample_Body', { main: 'User_Profile_Page_Example' });
   },
 });
 
@@ -12,6 +40,27 @@ FlowRouter.route('/list', {
   name: 'List_Stuff_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+  },
+});
+
+FlowRouter.route('/invites', {
+  name: 'List_Invites_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Invites_Page' });
+  },
+});
+
+FlowRouter.route('/projects', {
+  name: 'List_Projects_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Projects_Page' });
+  },
+});
+
+FlowRouter.route('/vendors', {
+  name: 'List_Vendors_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Vendors_Page' });
   },
 });
 
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -8,3 +8,24 @@ import './home-page.html';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
+
+import './log-in-page.html';
+import './log-in-page.js';
+
+import './landing-page.html';
+import './landing-page.js';
+
+import './user-profile.html';
+import './user-profile';
+
+import './list-invites-page.html';
+import './list-invites-page.js';
+
+import './list-projects-page.html';
+import './list-projects-page.js';
+
+import './list-vendors-page.html';
+import './list-vendors-page.js';
+
+import './user-profile-example.html';
+import './user-profile-example.js';
